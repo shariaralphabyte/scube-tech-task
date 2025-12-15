5513f9183bdda37b1ba7345c2dd02387c9fbc88f
shariaralphabyte <shariar@alphabytetech.com>
2025-12-15 16:55:52 +0600

    refactor: Remove unused CustomButton widget
    This commit removes the `CustomButton` widget, which is no longer in use within the project.
    
    The primary change is the deletion of the entire `CustomButton` class and its implementation from `lib/presentation/widgets/custom_button.dart`. A minor formatting adjustment was also made in `lib/presentation/widgets/custom_edit_text.dart`.

--- lib/presentation/widgets/custom_button.dart
+++ lib/presentation/widgets/custom_button.dart
@@ -3,67 +3,6 @@ import '../../core/theme/app_theme.dart';
 import '../../core/theme/text_style_helper.dart';
 import 'custom_image_view.dart';
 
-class CustomButton extends StatelessWidget {
-  final String text;
-  final VoidCallback? onPressed;
-  final bool isLoading;
-  final bool isPrimary;
-  final IconData? icon;
-  final double? width;
-
-  const CustomButton({
-    super.key,
-    required this.text,
-    this.onPressed,
-    this.isLoading = false,
-    this.isPrimary = true,
-    this.icon,
-    this.width,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: width ?? double.infinity,
-      height: 48,
-      child: ElevatedButton(
-        onPressed: isLoading ? null : onPressed,
-        style: ElevatedButton.styleFrom(
-          backgroundColor: isPrimary ? AppTheme.primaryBlue : AppTheme.white,
-          foregroundColor: isPrimary ? AppTheme.white : AppTheme.primaryBlue,
-          elevation: isPrimary ? 0 : 1,
-          side: isPrimary
-              ? null
-              : const BorderSide(color: AppTheme.primaryBlue, width: 1.5),
-        ),
-        child: isLoading
-            ? const SizedBox(
-                width: 20,
-                height: 20,
-                child: CircularProgressIndicator(
-                  strokeWidth: 2,
-                  valueColor: AlwaysStoppedAnimation<Color>(AppTheme.white),
-                ),
-              )
-            : Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  if (icon != null) ...[
-                    Icon(icon, size: 20),
-                    const SizedBox(width: 8),
-                  ],
-                  Text(text),
-                ],
-              ),
-      ),
-    );
-  }
-
-
-
-
-}
-
 class ShortcutItemWidget extends StatelessWidget {
   final String title;
   final String iconPath;
--- lib/presentation/widgets/custom_edit_text.dart
+++ lib/presentation/widgets/custom_edit_text.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+ import 'package:flutter/material.dart';
 
 import '../../core/app_export.dart';
 import './custom_image_view.dart';
